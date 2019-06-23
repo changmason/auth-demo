3c20c0897deeb5b488e89a037f822eb9b6931062
Mason <changmason@gmail.com>
2019-06-24 01:43:25 +0800

    Ensure email field is unique but case-insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, length: { minimum: 5 },  on: :update
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_uniqueness_of :email, case_sensitive: false
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }
 
   before_create :populate_initial_username
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username).on(:update) }
     it { should validate_length_of(:username).is_at_least(5).on(:update) }
     it { should validate_presence_of :email }
-    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of :password }
     it { should validate_confirmation_of :password }
     it { should validate_length_of(:password).is_at_least(8) }
