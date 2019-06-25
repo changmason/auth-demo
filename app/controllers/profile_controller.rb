2f7eb9ef9f93c10c72f891dd6bc497f7bedca851
Mason <changmason@gmail.com>
2019-06-25 21:47:51 +0800

    User can view and update profile
    - username can be updated
    - password can be updated
    - email is not updatable

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,4 +2,16 @@ class ProfileController < ApplicationController
   def show
     @user = User.last
   end
+
+  def update
+    @user = User.last
+    @user.update_attributes(user_params)
+    render :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
