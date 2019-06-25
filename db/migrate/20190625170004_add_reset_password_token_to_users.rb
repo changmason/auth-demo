fdc34797ee07da21a5eb4b84dde71683df063981
Mason <changmason@gmail.com>
2019-06-26 01:22:23 +0800

    Add reset_password_token fields to the users table

---
+++ db/migrate/20190625170004_add_reset_password_token_to_users.rb
@@ -0,0 +1,8 @@
+class AddResetPasswordTokenToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_token_expired_at, :datetime
+
+    add_index :users, :reset_password_token, unique: true
+  end
+end
