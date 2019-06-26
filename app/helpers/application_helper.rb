faa332477ffed32cb82b94b971fb429820f7aa9b
Mason <changmason@gmail.com>
2019-06-27 00:54:57 +0800

    User bootstrap alert to display validation errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
       </div>
     }.html_safe
   end
+
+  def display_errors(model)
+    messages = model.errors.full_messages.map { |msg| "<li>#{msg}</li>"}
+    %Q{
+      <div class="alert alert-danger" role="alert">
+        <ul>
+          #{messages.join}
+        </ul>
+      </div>
+    }.html_safe
+  end
 end
